d7cf1952553f01cb2f4810499313baaded074aac
umera779 <109315806+umera779@users.noreply.github.com>
2024-12-08 15:33:33 +0100

    Update wsgi.py

--- test_goat/wsgi.py
+++ test_goat/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for test_goat project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,5 +5,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_goat.settings')
 
 application = get_wsgi_application()
-app = application #add here
 
+app = application
